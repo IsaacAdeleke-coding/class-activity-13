2b88a7661fc0d5955639ef78462e08edd7074df1
IsaacAdeleke-coding <Isaacadelek3@gmail.com>
2024-09-16 11:24:06 -0700

    Initial Commit

---
+++ class activity 13/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  class activity 13
+//
+//  Created by Isaac Adeleke on 9/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
